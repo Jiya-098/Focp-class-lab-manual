5095de0a4784554f2cd3cac8757eb129a7bc483a
Jiya098 <jiya25csu098@ncuindia.edu>
2026-05-09 23:17:44 +0530

    Add BankAccount class with withdrawal functionality

---
+++ bank.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class BankAccount{  
+
+private:
+string accountHolderName;
+int balance;
+int rewardPoints;
+
+public:
+BankAccount(string name, double initialbalance, int reward) {
+accountHolderName = name;
+balance = initialbalance;
+rewardPoints = reward;
+}
+void withdraw(double amount, int point){
+    balance -= amount;
+    rewardPoints -= point;
+    cout<<"withdraw hogya"<<endl;
+}
+void displayAccount(){
+cout<<"NAME "<<accountHolderName<<endl;
+cout<<"Balance "<<balance<<endl;
+cout<<"REWARD POINTS "<<rewardPoints;
+}};
+
+int main(){
+
+BankAccount user1("neer",15000.0,1000);
+
+user1.withdraw(500.0,20);
+user1.displayAccount();
+}
